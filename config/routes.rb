a12490cd987f8f7dfcf419e993b394f14a0ea691
vagrant <vagrant@localhost.localdomain>
2020-05-15 13:28:56 +0000

    Add Activate Token

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   resources :items do
     resources :item_comments, only: [:create, :destroy]
   end
+  resources :account_activations, only: [:edit]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_15_020756) do
+ActiveRecord::Schema.define(version: 2020_05_15_122743) do
 
   create_table "item_comments", force: :cascade do |t|
     t.text "comment"
@@ -40,6 +40,9 @@ ActiveRecord::Schema.define(version: 2020_05_15_020756) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
